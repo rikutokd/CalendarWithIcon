ba954bdd32b09f8b30037e4d8344584550ea094f
Rikuto <rikutokd@yahoo.co.jp>
2020-01-15 00:51:40 +0900

    Initial Commit

---
+++ IconCalendar/CalculateCalendarLogic.swift
@@ -0,0 +1,13 @@
+public func judgeJapaneseHoliday(year: Int, month: Int, day: Int) -> Bool {
+
+    /**
+     *
+     * - Description judge japanese holiday
+     * - parameter year: year value (Int)
+     * - parameter month: month value (Int)
+     * - parameter day: day value (Int)
+     * - returns: result (Bool)
+     */
+
+     // judge holiday logic...
+}
