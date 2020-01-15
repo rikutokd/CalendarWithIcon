03dce7a512b00b5e3a0aa6852d36a4ec4e2caf5c
Rikuto <rikutokd@yahoo.co.jp>
2020-01-15 21:33:52 +0900

    + image on imageviewcontroller.swift

--- IconCalendar/ImageViewController.swift
+++ IconCalendar/ImageViewController.swift
@@ -1,5 +1,9 @@
 import UIKit
 
+    private let photos = ["Airplane",
+                          "Train",
+                          "Hotel",]
+
 class ImageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     override func viewDidLoad() {
@@ -7,36 +11,18 @@ class ImageViewController: UIViewController, UICollectionViewDelegate, UICollect
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 18 // 表示するセルの数
+        return photos.count // 表示するセルの数
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) // 表示するセルを登録(先程命名した"Cell")
-        cell.backgroundColor = .red  // セルの色
+        // Tag番号を使ってインスタンスをつくる
+                let photoImageView = cell.contentView.viewWithTag(1)  as! UIImageView
+                let photoImage = UIImage(named: photos[indexPath.row])
+               photoImageView.image = photoImage
+        
         return cell
     }
-    
-    
-//    private let photos = ["Airplane",
-//                          "Train",
-//                          "Hotel",]
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return photos.count
-//    }
-//
-//    // セル（要素）に表示する内容
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        // "Cell" の部分は　Storyboard でつけた cell の identifier。
-//        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-//
-//        // Tag番号を使ってインスタンスをつくる
-//        let photoImageView = cell.contentView.viewWithTag(1)  as! UIImageView
-//        let photoImage = UIImage(named: photos[indexPath.row])
-//        photoImageView.image = photoImage
-//
-//        return cell
-//    }
     
 }
 
