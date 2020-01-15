3273dc33163841e70a75f5149c339ca611f1c3aa
Rikuto <rikutokd@yahoo.co.jp>
2020-01-15 14:25:14 +0900

    + add  image view controller

---
+++ IconCalendar/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  IconCalendar
+//
+//  Created by 佐藤陸斗 on 2020/01/15.
+//  Copyright © 2020 hannet. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ IconCalendar/ImageViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+  
+
+    
+}
