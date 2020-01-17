5c434f99e7f4ea761eb91c90b7986005c25e2709
Rikuto <rikutokd@yahoo.co.jp>
2020-01-18 03:39:16 +0900

    + add realm class

---
+++ IconCalendar/EventModel.swift
@@ -0,0 +1,15 @@
+//
+//  EventModel.swift
+//  IconCalendar
+//
+//  Created by 佐藤陸斗 on 2020/01/18.
+//  Copyright © 2020 hannet. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class EventModel: Object {
+    @objc dynamic var date: String = ""
+    @objc dynamic var icon:
+}
