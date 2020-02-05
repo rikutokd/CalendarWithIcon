66803fb72a61c4e393954b2a644c7985693922e4
Rikuto <rikutokd@yahoo.co.jp>
2020-02-05 16:24:32 +0900

    + add toTextView segue & TextViewController

--- IconCalendar/TextViewController.swift
+++ IconCalendar/TextViewController.swift
@@ -2,11 +2,12 @@
 //  TextViewController.swift
 //  IconCalendar
 //
-//  Created by 佐藤陸斗 on 2020/01/17.
+//  Created by 佐藤陸斗 on 2020/02/05.
 //  Copyright © 2020 hannet. All rights reserved.
 //
 
 import UIKit
+import RealmSwift
 
 class TextViewController: UIViewController {
     
