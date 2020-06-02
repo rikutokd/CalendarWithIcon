fca98087f9bcb8be58ba06b783d691b02bb6b809
Rikuto <rikutokd@yahoo.co.jp>
2020-06-02 13:23:21 +0900

    value change ->HH時mm分  出力

--- GazouCalendar/SettingViewController.swift
+++ GazouCalendar/SettingViewController.swift
@@ -11,25 +11,31 @@ import UIKit
 class SettingViewController: UIViewController, UITextFieldDelegate{
     
     @IBOutlet weak var backBtn: UIButton!
+    //DatePicker設定
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //戻るボタンの設定
         backBtn!.addTarget(self, action: #selector(backEvent(_:)), for: .touchUpInside)
         
-        //DatePicker設定
-        let myDatePicker = UIDatePicker()
-        //DatePicker = 時間のみモード
-        myDatePicker.datePickerMode = UIDatePicker.Mode.time
-        // 分刻みを３０分単位で設定
-        myDatePicker.minuteInterval = 10
-        //DatePickerフレームサイズ設定
-        myDatePicker.frame = CGRect(x:16 , y: 80, width: 400, height: 200)
+        //機能追加
+        myDatePicker.addTarget(self, action: #selector(datePickerChanged(picker:)), for: .valueChanged)
         
         //viewにデートピッカー追加
         self.view.addSubview(myDatePicker)
     }
     
+}
+
+class DateUtils {
+    class func stringFromDate(date: Date, format: String) -> String {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.dateFormat = format
+        return formatter.string(from: date)
+    }
 }
 
 extension SettingViewController{
@@ -38,5 +44,9 @@ extension SettingViewController{
         //前のページに戻る
         self.dismiss(animated: true, completion: nil)
     }
-
+    
+    @objc func datePickerChanged(picker: UIDatePicker) {
+        print(DateUtils.stringFromDate(date: myDatePicker.date, format: "HH時mm分"))
+    }
+    
 }
